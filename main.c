e59f5cb73ac65bef0d63f5f9cb015114b7581523
ed system on behalf of Simon Dowd <ed>
2024-03-12 07:23:26 +0000

    Scaffold update by instructor

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "mtll.h"
 
 int main(int argc, char** argv) {
-    // TODO
-    // You should do general input / output management here.
+    TODO
+    You should do general input / output management here.
     return 0;
 }
